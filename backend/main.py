89244fdfff011986aaf5fddeea2a3f548a58a297
johnmtinezc <jonyedjo2@gmail.com>
2026-06-29 11:48:46 +0200

    chore: add comments in main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+# Inicializar FastAPI
 app = FastAPI()
 
+# Permite peticiones desde el frontend en desarrollo
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -10,7 +12,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+# Endpoint para comprobar que la API funciona correctamente
 @app.get("/health")
 async def health():
-    return {"status": "ok"}
+    return {"status": "Ok"}
